c82f61030c288c92a61c7746249394999a3ee141
HavocPigeon <speeas@gmail.com>
2018-06-20 21:48:36 -0700

    practice.final

--- classes.js
+++ classes.js
@@ -29,7 +29,19 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name,last_name,email,age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget(){
+    return this.first_name + " " + this.last_name + ' Widget'
+  }
+}
+const jim = new Employee('jim','boe','jb@gmail.com',15);
+console.log(jim);
 
 
 
@@ -49,7 +61,23 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager {
+  constructor(first_name,last_name,email,age,reports){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+  }
+  hire(employee){
+    this.reports.push(employee);
+    return this.reports;
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    return this.reports;
+  }
+}
 
 
 
@@ -75,7 +103,39 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager {
+  constructor(first_name,last_name,email,age,reports,title,bonus){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.reports = [];
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+  hire(employee){
+    this.reports.push(employee);
+    if (this.reports.length < 1){
+      this.title = 'Not a manager';
+    } else if (this.reports.length > 0 && this.reports.length <= 3){
+      this.title = 'Barely Manager';
+    } else if (this.reports.length > 3 && this.reports.length <= 10){
+      this.title = 'Mostly Manager';
+    } else if (this.reports.length > 10 && this.reports.length <= 50){
+      this.title = 'Manager';
+    } else if (this.reports.length > 50 && this.reports.length <= 100){
+      this.title = 'Manager Plus';
+    } else if (this.reports.length > 100){
+      this.title = 'Bestest Manager';
+    }
+    return this.title;
+  }
+  fire(index){
+    this.reports.splice(index, 1);
+    this.bonus += 100;
+    return this.title;
+  }
+}
 
 
 
@@ -102,6 +162,23 @@
         - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
-//Code Here
+class Machine {
+  constructor(widgets_made_count,wear_tear_count,needs_robot){
+    this.widgets_made_count = 0;
+    this.wear_tear_count = 0;
+    this.needs_robot = false;
+  }
+  makeWidgets(number){
+    this.widgets_made_count += number;
+    if (this.widgets_made_count % 50 === 0){
+      this.wear_tear_count += 1;
+    }
+    return this.widgets_made_count;
+  } 
+  fixMachine(){
+    this.needs_robot = true;
+    return this.needs_robot;
+  }
+}
 
 
--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  const objArray = [];
+  for (let prop in obj){
+   objArray.push(obj[prop]);
+  }
+  return objArray.join("");
 }
 
 
@@ -53,7 +57,14 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+const greaterThan10 = (obj) => {
+  for (let prop in obj){
+    if (obj[prop] > 10){
+      obj[prop] = 0
+    }
+  }
+  return obj;
+}
 
 
 
@@ -65,7 +76,12 @@ function showValues( obj ) {
   Return the updated object.
 */
 
-//Code Here
+const double = (obj) => {
+  for (let prop in obj){
+    obj[prop] *= 2;
+  }
+  return obj;
+} 
 
 
 
@@ -79,7 +95,15 @@ function showValues( obj ) {
   By the end of the for in loop, you should have a sentence, return that sentence.
 */
 
-//Code Here
+const secrets = (obj) => {
+  let emptyString = "";
+  for (let prop in obj){
+    if (prop.charAt(0) === "s" && prop.charAt(1) === "h"){
+      emptyString += obj[prop];
+    }
+  }
+  return emptyString;
+}
 
 
 
@@ -110,7 +134,10 @@ function showValues( obj ) {
   Delete the property password and return the object.
 */
 
-//Code Here
+const removePassword = (obj) => {
+  delete obj.password;
+  return obj;
+}
 
 
 
@@ -129,7 +156,15 @@ var deleteTheBigNumbers = {
   Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
 */
 
-//Code Here
+const deleteProps = (obj) => {
+  for (let key in obj){
+    if (obj[key] > 100){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+deleteProps(deleteTheBigNumbers);
 
 
 
@@ -142,7 +177,14 @@ var deleteTheBigNumbers = {
   Return the updated object.
 */
 
-//Code Here
+const startsWithK = (obj) => {
+  for (let key in obj){
+    if (key.charAt(0) === 'k'){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
 
 
 
@@ -157,6 +199,13 @@ var deleteTheBigNumbers = {
   (hint: the method includes() may be of use...)
 */
 
-//Code Here
+const hiddenTreasure = (obj) => {
+  for (let key in obj){
+  if (!obj[key].includes('treasure')){
+    delete obj[key];
+  }
+  }
+  return obj;
+}
 
 
